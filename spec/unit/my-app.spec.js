db02044605b978b2342a52ac258c578019278d36
filmaj <maj.fil@gmail.com>
2017-05-03 16:08:15 -0700

    adding pending specs for search functionality

--- spec/unit/my-app.spec.js
+++ spec/unit/my-app.spec.js
@@ -7,4 +7,16 @@ describe('my-app.js', function() {
       expect(pad2(11)).toEqual('11');
     });
   });
+
+  describe('searchSubmit function', function() {
+    var event;
+    beforeEach(function() {
+      event = jasmine.createSpyObj('fake event', ['preventDefault']);
+    });
+    it('it should always prevent default event propagation');
+    it('should alert if no formdata exists / was entered');
+    it('should make a request to the spotify API');
+    it('should load spotify API results into the main view router');
+    it('should alert if there was an error with the spotify API');
+  });
 });
