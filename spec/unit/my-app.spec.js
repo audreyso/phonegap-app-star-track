34b94cce36c76ca068f9db25972c2a8cdc6f563f
filmaj <maj.fil@gmail.com>
2017-05-03 13:52:00 -0700

    first stab

---
+++ spec/unit/my-app.spec.js
@@ -0,0 +1,7 @@
+describe('an important unit of my software', function() {
+    beforeEach(function() {
+    });
+    it('should do something, it really should.', function() {
+        expect(2).toEqual(4);
+    });
+});
