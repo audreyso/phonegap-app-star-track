72144cd809ea3d3f077fa23315bd9c9df38bcc78
filmaj <maj.fil@gmail.com>
2017-05-03 16:42:26 -0700

    use a spy on ajax to make sure it was invoked. also use a spy to fake the method into thinking proper form data was input.

--- spec/unit/my-app.spec.js
+++ spec/unit/my-app.spec.js
@@ -22,7 +22,12 @@ describe('my-app.js', function() {
       searchSubmit(event);
       expect(myApp.alert).toHaveBeenCalledWith('Please enter a search term', jasmine.any(String));
     });
-    it('should make a request to the spotify API');
+    it('should make a request to the spotify API', function() {
+      spyOn($$, 'ajax');
+      spyOn(myApp, 'formToJSON').and.returnValue({q: 'jetlagmakesmefeelfunny'});
+      searchSubmit(event);
+      expect($$.ajax).toHaveBeenCalled();
+    });
     it('should load spotify API results into the main view router');
     it('should alert if there was an error with the spotify API');
   });
