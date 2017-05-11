e4cdb703f86ed684588418510234a5b8e48aee66
filmaj <maj.fil@gmail.com>
2017-05-11 10:23:55 -0700

    basic nightwatch config for e2e tests.

---
+++ spec/e2e/nightwatch.conf.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  "src_folders" : ["spec/e2e/tests"],
+  "custom_commands_path" : "",
+  "custom_assertions_path" : "",
+  "page_objects_path" : "",
+  "selenium" : {
+    "start_process" : true,
+    "server_path" : "node_modules/selenium-server/lib/runner/selenium-server-standalone-3.4.0.jar",
+    "log_path" : "",
+    "port" : 4444,
+    "cli_args" : {
+      "webdriver.chrome.driver" : require('chromedriver').path
+    }
+  },
+  "test_settings" : {
+    "default" : {
+      "launch_url" : "http://localhost:3000",
+      "selenium_port"  : 4444,
+      "selenium_host"  : "localhost",
+      "silent": false,
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    }
+  }
+}
